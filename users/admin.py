2d3a1947d8210d0589a1857a12bb62c5eb6e96f6
Patrick <kimanthip@gmail.com>
2021-09-11 14:22:17 +0300

    register Profile, Gender and Tag

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Profile, Gender, Tag
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Gender)
+admin.site.register(Tag)
