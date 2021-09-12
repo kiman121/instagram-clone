051c7fad7187b96b18bcf56a309f167b8283e638
Patrick <kimanthip@gmail.com>
2021-09-12 08:39:13 +0300

    add loginUser, registerUser and explore view functions

--- users/views.py
+++ users/views.py
@@ -1,14 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.models import User
 
+from .models import Profile
+from .forms import CustomUserCreationForm
+
+from django.http import HttpResponse
 # Create your views here.
 
 
 def loginUser(request):
     page = 'login'
-    context = {
-        'page': page,
-    }
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'Username does not exist')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('explore')
+        else:
+            messages.error(request, 'Username or password is incorrect')
+
+    context = {'page': page}
     return render(request, 'users/login_register.html', context)
 
+
 def registerUser(request):
+    form = CustomUserCreationForm()
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+
+            messages.success(request, 'User account was created')
+
+            login(request, user)
+            return redirect('explore')
+        else:
+            messages.error(request, 'An error has occured during registration!')
+
     return render(request, 'users/login_register.html')
+
+
+@login_required(login_url='login')
+def explore(request):
+    return HttpResponse("you are in home page")
