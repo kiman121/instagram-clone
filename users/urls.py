93308228b1cd8f22af136fb1c91434b18490e367
Patrick <kimanthip@gmail.com>
2021-09-12 18:53:03 +0300

    add logout, profile and edit-profile paths

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.loginUser, name='login'),
     path('register/', views.registerUser, name='register'),
+    path('logout/', views.logoutUser, name='logout'),
+    path('profile/',views.userProfile, name='profile'),
+    path('edit-profile/',views.editProfile, name='edit-profile'),
 ]
