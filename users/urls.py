bf126435eebbf249bd48b306634c10f8ef647f34
Patrick <kimanthip@gmail.com>
2021-09-12 09:41:49 +0300

    rdelete the explore path

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.explore, name='explore'),
-    path('login', views.loginUser, name='login'),
+    path('', views.loginUser, name='login'),
     path('register/', views.registerUser, name='register'),
 ]
