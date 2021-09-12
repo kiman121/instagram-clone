379f275290235d10207f2bfbfb0f23e4970d9f1c
Patrick <kimanthip@gmail.com>
2021-09-12 08:37:32 +0300

    change the default route from login to explore

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.loginUser, name='login'),
+    path('', views.explore, name='explore'),
+    path('login', views.loginUser, name='login'),
     path('register/', views.registerUser, name='register'),
 ]
