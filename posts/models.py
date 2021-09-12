9ff22e0a39211e217efa9a1db4604fb02e335850
Patrick <kimanthip@gmail.com>
2021-09-12 18:46:28 +0300

    add post and tag models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,29 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+import uuid
 # Create your models here.
+
+
+class Post(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, null=True, blank=True)
+    
+    description = models.TextField(blank=True, null=True)
+    post_image = models.ImageField(upload_to='posts/')
+    tags = models.ManyToManyField('Tag', blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.description
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
