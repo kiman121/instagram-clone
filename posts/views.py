8c52de1ed03dc797f256f553181f8fcf2e98543f
Patrick <kimanthip@gmail.com>
2021-09-13 17:26:46 +0300

    add the upload post detail and explore view functions

--- posts/views.py
+++ posts/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.http import JsonResponse
 
 from users.models import Profile
+from .models import Post, Tag
+from .forms import PostForm
 
 # Create your views here.
 
@@ -11,8 +14,37 @@ from users.models import Profile
 @login_required(login_url='login')
 def explore(request):
     profile = request.user.profile
-
-    context = {'profile': profile}
+    posts = Post.objects.all()
+    context = {
+        'profile': profile,
+        'posts':posts,
+        'uploadForm': PostForm(),
+        'tags': Tag.objects.all(),
+    }
 
     return render(request, 'posts/explore.html', context)
 
+@login_required(login_url='login')
+def uploadPhoto(request):
+    user = request.user
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        upload = form.save(commit=False)
+        upload.user = user
+        upload.save()
+
+    return redirect(request.META.get('HTTP_REFERER'))
+
+@login_required(login_url='login')
+def postDetail(request,pk):
+    post = Post.objects.get(id=pk)
+    data = {
+        'id': post.id,
+        'post_image': post.post_image.url,
+        'description': post.description,
+        'tag': post.tag.name,
+        'user_image':post.user.profile.profile_image.url,
+        'user_name':post.user.profile.name,
+        'description': post.description,
+    }
+    return JsonResponse(data)
