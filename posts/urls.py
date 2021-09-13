a76c478ab05b7838406938bd4af5f76501b4a88a
Patrick <kimanthip@gmail.com>
2021-09-13 17:25:53 +0300

    add the upload-phot and post-detail paths

--- posts/urls.py
+++ posts/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.explore, name='explore'),
+    path('upload-photo/', views.uploadPhoto, name='upload-photo'),
+    # path('post-detail/<str:pk>/', views.postDetail, name='post-detail'),
+    path('post-detail/<str:pk>/', views.postDetail, name='post-detail'),
 ]
