60d8af12e865fc880eb4d598de968ee961ca864e
Patrick <kimanthip@gmail.com>
2021-09-14 10:34:16 +0300

    add comments model

--- posts/forms.py
+++ posts/forms.py
@@ -1,6 +1,6 @@
 from django.forms import ModelForm
 from django import forms
-from .models import Post
+from .models import Post,Comment
 
 class PostForm(forms.ModelForm):
     class Meta:
@@ -9,3 +9,14 @@ class PostForm(forms.ModelForm):
         widgets ={
             'tag': forms.Select()
         }
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['comment']
+    
+    def __init__(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'shadow-none bg-gray-100'})
