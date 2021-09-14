69d03f16ed67b1369c68a6ab1452d992c0c7220c
Patrick <kimanthip@gmail.com>
2021-09-14 23:07:14 +0300

    timeline, followuser and un follow path

--- posts/urls.py
+++ posts/urls.py
@@ -3,7 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.explore, name='explore'),
+    path('timeline', views.timeline, name='timeline'),
     path('upload-photo/', views.uploadPhoto, name='upload-photo'),
     path('post-detail/<str:pk>/', views.postDetail, name='post-detail'),
     path('add-comment/<str:pk>/', views.addComment, name='add-comment'),
+    path('add-like/<str:pk>/', views.addLike, name='add-like'),
+    path('follow-user/<str:followee_pk>/', views.followUser, name='follow-user'),
+    path('un-follow-user/<str:followee_pk>/', views.unFollowUser, name='un-follow-user'),
 ]
